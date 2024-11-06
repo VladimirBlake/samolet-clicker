b76030853156eca432e67479035b9dfd0dca559e
Vladimir Blake <mcpebyjohan@gmail.com>
2024-11-06 17:08:12 +0300

    feat: add custom font and set bg color

--- web-app/tailwind.config.ts
+++ web-app/tailwind.config.ts
@@ -9,8 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#007bfc",
+      },
+      fontFamily: {
+        cofo: "CoFo Sans",
       },
     },
   },

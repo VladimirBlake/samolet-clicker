c0c1be643f1ac064f94e93011be2319e70dc08c9
Vladimir Blake <mcpebyjohan@gmail.com>
2024-11-14 16:32:23 +0300

    feat: add improvement popup on main page

--- web-app/src/app/(application)/main-page/page.tsx
+++ web-app/src/app/(application)/main-page/page.tsx
@@ -1,16 +1,34 @@
+"use client";
 import { Page } from "@/components/Page";
 import BuildingData from "@/components/MainPage/BuildingData";
 import SpeedupButton from "@/components/MainPage/SpeedupButton";
+import ImprovementPopup from "@/components/Bonuses/ImprovementPopup";
+import { useState } from "react";
 
 export default function MainPage() {
+  const [isImprovementPopupShown, setIsImprovementPopupShown] = useState(false);
+
+  const hideImprovementPopup = () => {
+    setIsImprovementPopupShown(false);
+  };
+
+  const showImprovementPopup = () => {
+    setIsImprovementPopupShown(true);
+  };
+
   return (
     <Page back={false}>
       <div className="w-full h-full bg-[#3496FD] rounded-[30px]">
         <div className="py-6">
           <BuildingData />
-          <SpeedupButton />
+          <SpeedupButton onClick={showImprovementPopup} />
         </div>
       </div>
+      <ImprovementPopup
+        isShown={isImprovementPopupShown}
+        setNotShown={hideImprovementPopup}
+        improvementType="speed"
+      />
     </Page>
   );
 }

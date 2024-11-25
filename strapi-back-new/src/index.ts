efc268239f5a4c7244b9b22e484176d56749f60c
Vladimir Blake <mcpebyjohan@gmail.com>
2024-11-25 20:49:56 +0300

    feat: update advert schema and add sending to all users

--- strapi-back-new/src/index.ts
+++ strapi-back-new/src/index.ts
@@ -14,24 +14,50 @@ export default {
       if (context.uid !== "api::user-message.user-message") {
         return next();
       }
-
-      if (context.action === "create" && context.params.data?.telegram_user) {
-        const { chat_id } = await strapi
-          .documents("api::telegram-user.telegram-user")
-          .findOne({
-            documentId: context.params.data.telegram_user.connect[0].documentId,
-            fields: ["chat_id"],
-          });
-        if (chat_id) {
+      try {
+        if (context.params.data?.telegram_user) {
+          const { chat_id } = await strapi
+            .documents("api::telegram-user.telegram-user")
+            .findOne({
+              documentId:
+                context.params.data.telegram_user.connect[0].documentId,
+              fields: ["chat_id"],
+            });
+          if (chat_id) {
+            const bot = new Bot(strapi.config.get("server.telergamToken"));
+            await bot.api.sendMessage(
+              Number(chat_id),
+              context.params.data.message
+            );
+          }
+        } else {
+          const users = await strapi
+            .documents("api::telegram-user.telegram-user")
+            .findMany({
+              filters: {
+                chat_id: {
+                  $notNull: true,
+                },
+              },
+              status: "published",
+            });
           const bot = new Bot(strapi.config.get("server.telergamToken"));
-          await bot.api.sendMessage(
-            Number(chat_id),
-            context.params.data.message
+          await Promise.all(
+            users.map(async (user) => {
+              if (user?.chat_id) {
+                await bot.api.sendMessage(
+                  Number(user.chat_id),
+                  context.params.data.message
+                );
+              }
+            })
           );
         }
+      } catch (err) {
+        console.log(err);
+      } finally {
+        return next();
       }
-
-      return next();
     });
   },
 
